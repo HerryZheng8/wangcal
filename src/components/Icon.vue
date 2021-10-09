4f3f19c01a5d7a3fb6acc8815583162d6bf66687
HerryZheng <303983522@qq.com>
2021-10-09 21:56:43 +0800

    Icon-style

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,13 +1,26 @@
 <template>
-$END$
+  <svg class="icon">
+    <use v-bind:xlink:href="'#'+name"/>
+  </svg>
 </template>
 
-<script>
-export default {
-name: "Icon"
+<script lang="ts">
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icons", true, /\.svg$/));
+} catch (error) {
+  console.log(error);
 }
+export default {
+  props:['name'],
+  name: "Icon"
+};
 </script>
-
 <style scoped>
-
+  .icon{
+    width: 1em;height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
 </style>
