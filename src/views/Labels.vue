2abedb63bf5073e33ac94a9adf78f96e21eaa458
HerryZheng <303983522@qq.com>
2021-10-09 16:28:30 +0800

    创建重复三次布局

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,8 +1,11 @@
 <template>
- <div>
-   label
-   <Nav/>
- </div>
+  <div class="nav-wrapper">
+    <div class="content">
+      标签
+      <p>1</p>
+    </div>
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,5 +15,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    statistics
+  <div class="nav-wrapper">
+    <div class="content">
+      统计
+      <p>1</p>
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +15,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
